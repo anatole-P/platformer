ad8addeb67f3cee459ac47a846adc7dd9cd60c68
anatole-P <72392172+anatole-P@users.noreply.github.com>
2021-03-07 15:33:14 +0100

    soldats et hélicoptère

--- src/objects/Soldat.js
+++ src/objects/Soldat.js
@@ -1,29 +1,67 @@
 class Soldat extends ObjetEnnemi{
-
-    constructor(scene, x, y) {
+    /**
+     *
+     * @param {Tableau} scene
+     * @param x
+     * @param y
+     */
+     constructor(scene, x, y) {
         super(scene, x, y, "soldat");
-
-        
-        this.setOrigin(0,0);
-        this.setDisplaySize(64,64);
+        //pas de gravité
+        this.body.allowGravity=true;
         this.setCollideWorldBounds(true);
-        this.setBounce(0);
-        this.setVelocityX(50);
-        this.physics.add.overlap(this.player, this.Soldat, this.hitSpike, null, this);
-        
+        //gestion de la taille...car attention notre png est très grand (et c'est maaaaal car pas optimisé)
+        this.setDisplaySize(64,64);
+        this.setBounceX(1);
+        //on réduit un peu la zone de hit
+        this.setBodySize(this.body.width,this.body.height);
+        //this.setOffset(150, 250);
 
-        
+        //définir les propriété que l'on va utiliser dans notre animation
+
+        // X
+        this.originalX=x;
+        this.minX=x-200;
+        this.maxX=x+200;
+
+
+
+        // on applique les propriétés du début de l'animation
+        this.x=this.minX;
+        this.alpha=0;
+        let me=this;
+
+        //on fait apparaitre notre objet avec un petit delay, puis on lance l'animation
+        //ceci a pour effet de décaler les animations pour ce même objet
+        scene.tweens.add({
+                targets:this,
+                duration:200,
+                delay:Math.random()*1000,
+                alpha:{
+                    startDelay:Math.random()*5000,
+                    from:0,
+                    to:1,
+                },
+                onComplete: function () {
+                    me.start();
+                }
+            })
+
     }
 
-    update(){
-        //fait changer de sens notre soldat
-        if(this.body){
-            if(this.body.velocity.x<0){
-                this.flipX=true;
-            }else{
-                this.flipX=false;
-            }
-        }
-    
+    start(){
+        this.scene.tweens.add({
+            targets: this,
+            x: {
+                from: this.minX,
+                to:this.maxX,
+                duration: 10*500,
+                ease: 'Sine.easeInOut',
+                yoyo: -1,
+                repeat:-1,
+                flipX:true,
+            },
+        });
     }
+
 }
